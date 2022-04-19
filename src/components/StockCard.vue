1d422cd9612e5a0b63f77e4f13cc1e3e16afb24a
Pony <ponyma2100@gmail.com>
2022-04-19 23:03:09 +0800

    Add css style

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -1,16 +1,33 @@
 <template>
   <div class="cards">
     <div class="card" v-for="stock in stockList" :key="stock.rowId">
-      <div class="stock-name">
-        <p>{{ stock.symbolName }}</p>
+      <div class="card-left">
+        <div class="stock-name">
+          <p>{{ stock.symbolName }}</p>
+        </div>
+        <div class="stock-id">
+          <p>{{ stock.symbol }}</p>
+        </div>
       </div>
-      <div class="id">
-        <p>{{ stock.symbol }}</p>
+      <div class="card-center">
+        <button class="stock-price">
+          {{ stock.previousClose }}
+        </button>
+        <div class="stock-vol">
+          <p><i class="fas fa-sort-up"></i>{{ stock.change }}</p>
+          <!-- <p><i class="fas fa-sort-down"></i>{{ stock.change }}</p> -->
+          <p>{{ stock.volK }}</p>
+        </div>
       </div>
-      <div class="price">
-        <p>{{ stock.previousClose }}</p>
+      <div class="card-right">
+        <div class="stock-changePercent">
+          <span>{{ stock.changePercent }}</span>
+        </div>
+        <div class="stock-hl">
+          <span class="high">H: {{ stock.dayHigh }}</span>
+          <span class="low">L: {{ stock.dayLow }}</span>
+        </div>
       </div>
-      <button class="btn">下單</button>
     </div>
   </div>
 </template>
@@ -29,5 +46,78 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+.card {
+  background: var(--dark-secondary);
+  /* background: #263544; */
+  margin: 5px;
+  min-width: 360px;
+  display: flex;
+  justify-content: space-evenly;
+  border-radius: var(--border-radius);
+  height: 100px;
+}
+
+.card-left,
+.card-center,
+.card-right {
+  width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.card-left {
+  align-items: start;
+}
+
+.card-center {
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+.card-right {
+  align-items: flex-end;
+}
+
+.stock-price {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: var(--dark-primary);
+  border-radius: var(--border-radius);
+  width: 100px;
+  height: 40px;
+  min-width: 60px;
+  border: 0;
+  color: var(--stock-up);
+  font-size: 16px;
+}
+
+.stock-price:hover {
+  filter: opacity(0.8);
+}
+
+.stock-vol {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+.stock-vol p {
+  display: flex;
+}
+
+.stock-hl {
+  display: flex;
+  flex-direction: column;
+}
+.high {
+  color: var(--stock-up);
+}
+.low {
+  color: var(--stock-down);
+}
+
+.stock-id {
+  font-size: 12px;
+}
 </style>
