23059ba837a3d42df494fed4699c78127a29cf6e
Pony <ponyma2100@gmail.com>
2022-04-29 22:47:17 +0800

    Modify api url

--- src/composables/getStock.js
+++ src/composables/getStock.js
@@ -1,6 +1,5 @@
 import { ref } from "vue"
-let BASE_URL = `https://tw.stock.yahoo.com/_td-stock/api/resource/StockServices.stockList;fields=avgPrice%2Corderbook;symbols=`
-const corsURL = 'https://cors-anywhere.herokuapp.com/'
+
 const getStock = () => {
 
   let stockInfo = ref([])
@@ -8,18 +7,12 @@ const getStock = () => {
 
   const getStockInfo = async (symbol = '2330') => {
 
-    const STOCK_URL = BASE_URL + `${symbol + '.TW'}`
-
     try {
-      const response = await fetch(`${corsURL
-        }${STOCK_URL}`, {
-        method: 'get',
-        headers: { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'content-type': 'application/json', 'Access-Control-Allow-Credentials': 'true', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36' }
-      })
-
+      const response = await fetch(`https://quiet-brushlands-71126.herokuapp.com/api/stockinfo/${symbol}`)
       const data = await response.json()
 
       stockInfo.value = await data
+      console.log("🚀 ~ file: getStock.js ~ line 16 ~ getStockInfo ~ stockInfo.value", stockInfo.value)
 
     } catch (error) {
       console.log(error)
@@ -32,15 +25,8 @@ const getStock = () => {
 
   const getOtcInfo = async (symbol = '8069') => {
 
-    const OTC_URL = BASE_URL + `${symbol + '.TWO'}`
-
     try {
-      const response = await fetch(`${corsURL
-        }${OTC_URL}`, {
-        method: 'get',
-        headers: { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'content-type': 'application/json', 'Access-Control-Allow-Credentials': 'true', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36' }
-      })
-
+      const response = await fetch(`https://quiet-brushlands-71126.herokuapp.com/api/otcinfo/${symbol}`)
       const data = await response.json()
 
       otcInfo.value = await data
--- src/composables/getStocks.js
+++ src/composables/getStocks.js
@@ -1,9 +1,5 @@
 import { ref } from "vue"
 
-const BASE_URL = 'https://tw.stock.yahoo.com/_td-stock/api/resource/StockServices.rank;exchange=TAI;limit=100;offset=0;period=1D;sortBy=-volume'
-const OTC_URL = 'https://tw.stock.yahoo.com/_td-stock/api/resource/StockServices.rank;exchange=TWO;limit=100;offset=0;period=1D;sortBy=-volume'
-const corsURL = 'https://cors-anywhere.herokuapp.com/'
-
 // https://tw.stock.yahoo.com/_td-stock/api/resource/StockServices.rank;exchange=TAI;limit=100;offset=0;period=1D;sortBy=-volume
 
 const getStocks = () => {
@@ -11,15 +7,11 @@ const getStocks = () => {
   let stockList = ref([])
   let otcList = ref([])
 
+  // get stocks
   const getStockList = async () => {
     try {
-      const response = await fetch(`${corsURL
-        }${BASE_URL}`, {
-        method: 'get',
-        headers: { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'content-type': 'application/json', 'Access-Control-Allow-Credentials': 'true', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36' }
-      })
-      const data = await response.json()
-      stockList.value = await data.list
+      const response = await fetch('https://quiet-brushlands-71126.herokuapp.com/api/volumestocks')
+      stockList.value = await response.json()
 
     } catch (error) {
       console.log(error)
@@ -29,15 +21,11 @@ const getStocks = () => {
 
   }
 
+  //  get otc stocks
   const getOtcList = async () => {
     try {
-      const response = await fetch(`${corsURL
-        }${OTC_URL}`, {
-        method: 'get',
-        headers: { 'Access-Control-Allow-Headers': '*', 'Access-Control-Allow-Origin': '*', 'content-type': 'application/json', 'Access-Control-Allow-Credentials': 'true', 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/111.25 (KHTML, like Gecko) Chrome/99.0.2345.81 Safari/123.36' }
-      })
-      const data = await response.json()
-      otcList.value = await data.list
+      const response = await fetch('https://quiet-brushlands-71126.herokuapp.com/api/volumeotcstocks')
+      otcList.value = await response.json()
 
     } catch (error) {
       console.log(error)
@@ -50,6 +38,5 @@ const getStocks = () => {
   return { getStockList, getOtcList, stockList, otcList }
 }
 
-// getStocks()
 
 export default getStocks
